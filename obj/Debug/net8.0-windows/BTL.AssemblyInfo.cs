a8ef27f6230bac4385143d84f50d468fe40b55ab
CaoMinh2110 <khuatcaominh.2110@gmail.com>
2025-06-22 18:21:53 +0700

    QuanLyDiemSinhVien

--- obj/Debug/net8.0-windows/BTL.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/BTL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b414ee79bc9f7b6de57e455863fb70a6e81dcb6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
